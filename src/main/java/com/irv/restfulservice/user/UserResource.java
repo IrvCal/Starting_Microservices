7d702b55082aee804e1c61d14bfa1cee87003d69
IrvCal <juanirvincalderon@gmail.com>
2021-10-15 10:55:15 -0500

    Implementacion de la dependencia de Swagger al proyecto para que brinde la interfaz grafica de swagger

--- src/main/java/com/irv/restfulservice/user/UserResource.java
+++ src/main/java/com/irv/restfulservice/user/UserResource.java
@@ -14,7 +14,7 @@ import java.util.Iterator;
 import java.util.List;
 
 @RestController
-public class UserResource {
+public class UserResource {//Se puede llamar Resource O Controller
     @Autowired
     private UserDaoService userDaoService;
 
