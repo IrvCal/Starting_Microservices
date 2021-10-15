a7bf2936fa56ae1667a5f471878ca802cf773fbc
IrvCal <juanirvincalderon@gmail.com>
2021-10-15 09:26:33 -0500

    Internacionalización de las diferentes frases que se pueden desplegar de acuerdo al lenguaje que este esn el header

--- src/main/java/com/irv/restfulservice/helloworld/HelloWorldController.java
+++ src/main/java/com/irv/restfulservice/helloworld/HelloWorldController.java
@@ -1,10 +1,20 @@
 package com.irv.restfulservice.helloworld;
 
 import com.irv.restfulservice.helloworld.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController//cual es la diferencia entre RestController y este
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
+
 
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
@@ -17,5 +27,18 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
         return new HelloWorldBean(String.format("Hello World, %s",name));
+    }
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(
+//            @RequestHeader(name = "Accept-Language", required = false)Locale locale
+//            se supone que no es ta tan bien estar poniendo esto en todos los metodos que
+//            ocpen de esto, es mejor llamar a la clase LocaleContextHolder
+            ){
+        return messageSource
+                .getMessage("good.morning.message"
+                        ,null
+                        ,"Default massage"
+//                        ,locale);
+                        , LocaleContextHolder.getLocale());
     }
 }
